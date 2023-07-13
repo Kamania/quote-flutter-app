3472169ec2e54079e7a74a6d573b8279be51f8a6
Kamania <kamaniajoseph@gmail.com>
2023-07-13 11:41:30 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple flutter app to show quotes
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(
+        author: "Wilde",
+        text:
+            "In the midst of challenges, lies the opportunity for growth and transformation."),
+    Quote(
+        author: "Wilde",
+        text:
+            "The power to change resides within each one of us; it's a matter of embracing it."),
+    Quote(
+        author: "Wilde",
+        text:
+            "Happiness is not a destination, but a state of mind cultivated through gratitude and self-love."),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Awesome Quotes"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes
+            .map((quote) => QuoteCard(
+                quote: quote,
+                delete: () {
+                  setState(() {
+                    quotes.remove(quote);
+                  });
+                }))
+            .toList(),
+      ),
+    );
+  }
+}
